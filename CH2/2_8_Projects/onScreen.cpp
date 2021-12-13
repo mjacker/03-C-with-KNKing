63d2e82e4892763212d264cb0a9d1242df91ebe4
Jaime Colman <mjackerjaime@gmail.com>
2021-12-12 22:24:31 -0300

    Starting with 2.8 Projects, Added, print to console.

---
+++ CH2/2_8_Projects/onScreen.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>  // needed for printf
+#include <iostream> // needed for cout, cin.
+
+using namespace std; // needed for cout, cin.
+
+int main(void)
+{
+
+    printf("- Hello with printf\n"); // Lenguage C
+    cout << " Hello with cout \n";  // Language Cpp
+
+}
